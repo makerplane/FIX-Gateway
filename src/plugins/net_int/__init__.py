f0cd7bcc5e691738d9080d2a0c748b720514e0b1
Phil Birkelbach <phil@petrasoft.net>
2016-05-20 14:23:42 -0500

    A bit of cleanup

--- src/plugins/net_int/__init__.py
+++ src/plugins/net_int/__init__.py
@@ -17,6 +17,10 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
+# This is a networked version of the command line plugin.  They probably
+# need to be merged into the same plugin and configuration used to determine
+# whether to use the network or prompt.
+
 import plugin
 import threading
 import socket
