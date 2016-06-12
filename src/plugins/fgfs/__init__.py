92bd95a35a69575b0bd7a74750635032617466bb
Phil Birkelbach <phil@petrasoft.net>
2016-06-12 15:45:11 -0500

    Fix a bug where the run function was overwritten in fgfs plugin.

--- src/plugins/fgfs/__init__.py
+++ src/plugins/fgfs/__init__.py
@@ -38,7 +38,7 @@ class UDPClient(threading.Thread):
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.sock.settimeout(2.0)
         self.sock.bind((UDP_IP, UDP_PORT))
-        self.run = 1
+        self.getout = False
 
     def write_data(self, data):
         l = data.split(var_sep)
@@ -48,7 +48,7 @@ class UDPClient(threading.Thread):
 
     def run(self):
         buff = ""
-        while self.run:
+        while not self.getout:
             #Reads the UDP packet splits then sends it to the Queue
             try:
                 data = self.sock.recv(1024)  # buffer size is 1024 bytes
@@ -61,9 +61,10 @@ class UDPClient(threading.Thread):
                             buff = ""
             except socket.timeout:
                 pass
+        self.running = False
 
     def stop(self):
-        self.run = 0
+        self.getout = 0
 
 
 class Item(object):
