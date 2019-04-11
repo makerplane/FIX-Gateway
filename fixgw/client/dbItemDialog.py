32f46981096c1bd83a50935c9742a3655123fe47
Phil Birkelbach <phil@petrasoft.net>
2019-04-11 10:57:27 -0500

    Improve aux data handling in gui client dbitem dialog

--- fixgw/client/dbItemDialog.py
+++ fixgw/client/dbItemDialog.py
@@ -80,9 +80,8 @@ def getValueControl(item, parent, signals = True):
 
 
 # Returns a function that can be used as a slot for aux value changes
-def auxSlotClosure(item, auxname):
+def auxValueSlotClosure(item, auxname):
     def func(value):
-        print('Aux Closure')
         item.set_aux_value(auxname, value)
     return func
 
@@ -92,6 +91,34 @@ class ItemDialog(QDialog, itemDialog_ui.Ui_Dialog):
         super(ItemDialog, self).__init__(*args, **kwargs)
         self.setupUi(self)
         self.setAttribute(Qt.WA_DeleteOnClose)
+
+    def auxAddClosure(self, auxname):
+        def func():
+            none_label = self.aux_controls[auxname]["none"]
+            control = self.aux_controls[auxname]["control"]
+            del_btn = self.aux_controls[auxname]["del_btn"]
+            add_btn = self.aux_controls[auxname]["add_btn"]
+            add_btn.hide()
+            del_btn.show()
+            none_label.hide()
+            control.show()
+            self.item.set_aux_value(auxname, 0.0)
+        return func
+
+    def auxDelClosure(self, auxname):
+        def func():
+            none_label = self.aux_controls[auxname]["none"]
+            control = self.aux_controls[auxname]["control"]
+            del_btn = self.aux_controls[auxname]["del_btn"]
+            add_btn = self.aux_controls[auxname]["add_btn"]
+            add_btn.show()
+            del_btn.hide()
+            none_label.show()
+            control.hide()
+            self.item.set_aux_value(auxname, None)
+
+        return func
+
 
     def setKey(self, key):
         self.key = key
@@ -175,12 +202,53 @@ class ItemDialog(QDialog, itemDialog_ui.Ui_Dialog):
         self.formLayout.addRow(l, r)
 
         aux_list = self.item.get_aux_list()
+        self.aux_controls = {}
         for aux in aux_list:
+            d = {}
+            l = QLabel(self.scrollAreaWidgetContents)
+            l.setText(aux)
+
+            vc = getValueControl(self.item, self.scrollAreaWidgetContents, False)
+            vc.valueChanged.connect(auxValueSlotClosure(self.item, aux))
+            vc.hide()
+            d["control"] = vc
+
+            vl =  QLabel(self.scrollAreaWidgetContents)
+            vl.setText("None")
+            d["none"] = vl
+
+            del_icon = QIcon.fromTheme("list-remove")
+            del_btn = QPushButton()
+            del_btn.setIcon(del_icon)
+            del_btn.hide()
+            del_btn.clicked.connect(self.auxDelClosure(aux))
+            d["del_btn"] = del_btn
+            add_icon = QIcon.fromTheme("list-add")
+            add_btn = QPushButton()
+            add_btn.setIcon(add_icon)
+            add_btn.clicked.connect(self.auxAddClosure(aux))
+            d["add_btn"] = add_btn
+            self.aux_controls[aux] = d
+
+            box = QHBoxLayout()
+            box.addWidget(vl)
+            box.addWidget(vc)
+            box.addWidget(del_btn)
+            box.addWidget(add_btn)
             v = self.item.get_aux_value(aux)
             if v is not None:
-                l = QLabel(self.scrollAreaWidgetContents)
-                l.setText(aux)
-                r = getValueControl(self.item, self.scrollAreaWidgetContents, False)
-                r.setValue(v)
-                r.valueChanged.connect(auxSlotClosure(self.item, aux))
-                self.formLayout.addRow(l, r)
+                vc.setValue(v)
+                del_btn.show()
+                add_btn.hide()
+                vl.hide()
+                vc.show()
+            self.formLayout.addRow(l, box)
+
+# TODO:
+#   When an aux item is None it is displayed as a label that says 'None.'  There
+#   should be a click event that will change it to a spinBox so that it can
+#   be edited.  None is different that zero.  Once the spinbox comes up we should
+#   have a delete 'x' also to set the aux back to None.  This feature probably
+#   needs to be added to netfix as well.
+
+#   auxChanged signal from the item needs to be implemented
