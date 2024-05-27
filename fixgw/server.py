9e7f54dddbd9d5c4f2fc6d5178b070923d1fa524
Eric Blevins <eblevins@kmilearning.com>
2024-05-26 20:30:52 -0400

    added fixid GATEWAY_VERSION and PYEFIS_VERSION, set GATEWAY_VERSION at startup

--- fixgw/server.py
+++ fixgw/server.py
@@ -15,7 +15,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-
+from fixgw import version
 import yaml
 try:
     import queue
@@ -198,6 +198,7 @@ def main_setup():
         log.error("Database failure, Exiting:" + str(e))
         raise
 
+    database.write('GATEWAY_VERSION', version.VERSION)
     if "initialization files" in config and config["initialization files"]:
         ifiles = config["initialization files"]
         for fn in ifiles:
