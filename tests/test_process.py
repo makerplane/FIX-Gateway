411a45354ea1e6a4300a733eef68c7cfaa5f16bb
Eric Blevins <eblevins@kmilearning.com>
2025-01-06 21:49:24 -0500

    Fixed some tests to work with recent changes

--- tests/test_process.py
+++ tests/test_process.py
@@ -31,7 +31,7 @@ class TestProcess(unittest.TestCase):
 
     def test_MinimalSuccess(self):
         """Minimal Process start/stop test"""
-        p = subprocess.Popen(["python3", "fixgw.py", "--debug", "--config-file", "tests/config/minimal.yaml"])
+        p = subprocess.Popen(["python3", "fixGw.py", "--debug", "--config-file", "tests/config/minimal.yaml"])
         time.sleep(0.3)
         p.terminate()
         x = p.wait()
