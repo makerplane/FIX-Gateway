33bd244cb4777938cb69aee733d2124d32195833
Eric Blevins <eblevins@kmilearning.com>
2025-01-06 20:55:47 -0500

    Move to pyproject.toml to match pyefis

--- fixGw.py
+++ fixGw.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 #!/usr/bin/env python3
 
 #  Copyright (c) 2018 Phil Birkelbach
@@ -17,5 +16,5 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-from fixgw import server
+import fixgw.server as server
 server.main()
